9708e9bdb8cd9f25f02213a084c67bdbf6936993
Cindy Jelinsky <cindyjelinsky@yahoo.com.br>
2024-10-02 11:19:05 -0300

    login-window

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
